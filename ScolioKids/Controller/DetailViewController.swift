343ebfc2f1ffdaa707c424184455967fcebd6c0f
anneauzier <anne.auziier@outlook.com>
2022-06-03 14:35:43 -0400

    coloquei a outra view

--- ScolioKids/Controller/DetailViewController.swift
+++ ScolioKids/Controller/DetailViewController.swift
@@ -8,11 +8,41 @@
 import Foundation
 import UIKit
 
-class DetailViewController: UIViewController{
+class DetailViewController: UIViewController {
     
     @IBOutlet weak var InstructionView: UITableView!
     
+    var nameTrainning: String = ""
+    var dicEx: [String : [listaInst]] = [
+        "Lombar" : [
+            listaInst(textoCelula: "Passo 1: Você deve primeiro flexionar os braços", imageCelula: "outralombar"),
+            listaInst(textoCelula: "Passo 2: Agora você deve estender as pernas", imageCelula: "outralombar2"),
+        ],
+        "Cervical" : [
+            listaInst(textoCelula: "Passo 1: Agora você deve levantar a cabeça", imageCelula: "outracervical"),
+            listaInst(textoCelula: "Passo 2: Agora você deve girar o tronco sla", imageCelula: "outracervical2")
+        ],
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        InstructionView.delegate = self
+        InstructionView.dataSource = self
+    }
+}
+extension DetailViewController: UITableViewDelegate, UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dicEx[nameTrainning]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let newcell = InstructionView.dequeueReusableCell(withIdentifier: "newcell", for: indexPath) as! CustomNewCell
+        
+        if let arrayEx = dicEx[nameTrainning] {
+            newcell.imageCell.image = UIImage(named: arrayEx[indexPath.row].imageCelula)
+            newcell.textCell.text = arrayEx[indexPath.row].textoCelula
+        }
+        return newcell
     }
 }
