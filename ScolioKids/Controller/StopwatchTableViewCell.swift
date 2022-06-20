38c235ad05985d815b2fb4ddd4beaafae0e1fbdb
dessanagit <dessanacms@Dessanas-MacBook-Pro.local>
2022-06-20 13:48:10 -0400

    Add Stopwatch

--- ScolioKids/Controller/StopwatchTableViewCell.swift
+++ ScolioKids/Controller/StopwatchTableViewCell.swift
@@ -7,19 +7,41 @@
 
 import UIKit
 
-class StopwatchTableViewCell: UITableViewCell {
+class StopwatchTableViewCell: UITableViewCell, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    
-    
+//    @IBOutlet weak var pickerVie
+    @IBOutlet weak var pickerViewNumbers: UIPickerView!
+    var pickerNumbers = Array(0...30)
+
+   
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+        pickerViewNumbers.delegate = self
+//        pickerViewNumbers.dataSource = self
+        
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerNumbers.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let pickerNumber = "\(pickerNumbers[row])"
+        
+        return pickerNumber
     }
 
 }
--- ScolioKids/Controller/StopwatchTableViewController.swift
+++ ScolioKids/Controller/StopwatchTableViewController.swift
@@ -6,85 +6,107 @@
 //
 
 import UIKit
+import CoreData
 
 class StopwatchTableViewController: UITableViewController {
-
+    
+    
     @IBOutlet var stopwatchUI: UITableView!
+    
+    var chronometer: [NSManagedObject] = []
+    
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+
+    let context: NSManagedObjectContext = {
+        let container = NSPersistentContainer(name: "coredata")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("erro de load \(error)")
+            }
+        }
+        return container.viewContext
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+    }
+    
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    // MARK: - Table view data source
+
+    override func viewWillAppear(_ animated: Bool) {
+        //2
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Chronometer")
+        
+        //3
+        do {
+          chronometer = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+          print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        stopwatchUI.delegate = self
+        stopwatchUI.dataSource = self
+        stopwatchUI.reloadData()
+  
     }
 
     // MARK: - Table view data source
 
+  
+//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//
+//    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+//
+//
+//        let deleteAction = UIContextualAction(style: .destructive, title:  "Delete", handler:{[self] (_, _, completionHandler) in
+//            // delete the item here
+//
+//            context.delete(self.chronometer[indexPath.row])
+//            self.chronometer.remove(at: indexPath.row)
+//            // Save Changes
+//            appDelegate!.saveContext()
+//            // Remove row from TableView
+//            self.stopwatchUI.deleteRows(at: [indexPath], with: .left)
+//
+//            completionHandler(true)
+//
+//        })
+//
+//        let action = UISwipeActionsConfiguration(actions: [deleteAction])
+//
+//        return action
+//
+//
+//    }
+//
+    
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
+//        return chronometer.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
+//        let chronometerExercise = chronometer[indexPath.row]
+        
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reusebleChronometerCell", for: indexPath)
+        cell.textLabel?.text = "hello world"
+//            cell.textLabel?.text = chronometerExercise.value(forKeyPath: "chronometerName") as? String
+    
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
